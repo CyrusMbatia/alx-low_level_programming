4a1e20c4d892b349140835dad6484d9258d94201
CyrusMbatia <cyrusmbatiah@gmail.com>
2022-07-05 14:55:07 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,23 @@
  * Returns -1 if n is less than zero and print -1
  */
 int print_sign(int n)
+{
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-
 	}
+
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-
 	}
+
 	else
 	{
 		_putchar('-');
 		return (-1);
-
 	}
 }
+
