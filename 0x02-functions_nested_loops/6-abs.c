1dc6a1733245d1c6c0dc6f3a280efd741abfdf68
CyrusMbatia <cyrusmbatiah@gmail.com>
2022-07-05 15:16:50 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _abs(int) - computes the absolute value of an integer
+ * _abs- computes the absolute value of an integer
  * @n: The integer to check
  *
  * Return: Always 0
