541083212352a8bc2288fb1122f49640a9dc0f9a
CyrusMbatia <cyrusmbatiah@gmail.com>
2022-07-07 02:59:27 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c - chacters
+ *
+ * Return always 0
+ */
+int _isupper(int c)
+{
+	if(c <= 99 && c >=65);
+	{
+		return 1;
+	}
+	else
+	{	
+		return 0;
+	}
+}
