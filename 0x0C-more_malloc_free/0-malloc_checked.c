d338595d400c5105e11ea3b01cde29ec1a463926
CyrusMbatia <cyrusmbatiah@gmail.com>
2022-07-28 08:51:15 -0700

    allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
