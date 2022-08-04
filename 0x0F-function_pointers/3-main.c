de431049d870fceec7488a6a9ce66ebba48708a3
CyrusMbatia <cyrusmbatiah@gmail.com>
2022-08-04 00:18:56 -0700

    performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,37 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int (*operation)(int, int);
+	register int a, b;
+	int (*fptr)(int, int);
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
-
-	printf("%d\n", operation(i, j));
+		printf("Error\n"), exit(98);
+	fptr = get_op_func(argv[2]);
+	if (!fptr)
+		printf("Error\n"), exit(99);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%i\n", fptr(a, b));
 	return (0);
 }
