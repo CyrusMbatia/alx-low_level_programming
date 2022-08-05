ac9ab7a3c5fc8ac4d08e39878e1e06bd73362d0f
CyrusMbatia <cyrusmbatiah@gmail.com>
2022-08-05 03:11:49 -0700

    main function only

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,23 +1,39 @@
 #include "3-calc.h"
 
 /**
- * main - check the code
- * @argc: argument count
+ * main - program entry.
+ * @argc: argument counter.
  * @argv: argument vector
- * Return: Always 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	register int a, b;
-	int (*fptr)(int, int);
+	int a = 0, b = 0, result = 0;
+	char s;
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
-	fptr = get_op_func(argv[2]);
-	if (!fptr)
-		printf("Error\n"), exit(99);
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	s = argv[2][0];
+
+	if (s != '+' && s != '-' && s != '*' && s != '/' && s != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	printf("%i\n", fptr(a, b));
+	result = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", result);
 	return (0);
 }
