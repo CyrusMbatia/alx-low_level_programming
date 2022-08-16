835777762f1ecab1f6d8523a7750712a3da6a4c0
CyrusMbatia <cyrusmbatiah@gmail.com>
2022-08-16 03:55:24 -0700

    finds the loop in a linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -5,7 +5,7 @@
  * @head: linked list to search for
  * Return: address of the node where the loop starts, or NULL
  */
-istint_t *find_listint_loop(listint_t *head)
+listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
 	listint_t *fast = head;
